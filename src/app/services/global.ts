38f7198d59e3c0d4e56d77d55a881b1cd462e073
Bruna Reis <bruna_reissv@hotmail.com>
2019-12-01 22:06:35 -0300

    Correção URL

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,7 @@
 
 export abstract class Global {
-    protected readonly url = 'http://localhost:3000'; // `https://beer-api-bruna.herokuapp.com`;
+    // protected readonly url = 'http://localhost:3000';
+    protected readonly url = 'https://beer-api-bruna.herokuapp.com';
 
     constructor() {  }
 }
