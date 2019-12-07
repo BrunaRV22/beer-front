60caf31b8081c591de538f02a0d1dad68c2de6bc
Bruna Reis <bruna_reissv@hotmail.com>
2019-12-07 16:06:22 -0300

    Correção para mudar de rota quando inativo

--- src/app/pages/pedidos/pedidos-resolver.service.ts
+++ src/app/pages/pedidos/pedidos-resolver.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { PedidosService } from 'src/app/services/pedidos.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class PedidosResolverService implements Resolve<any> {
     constructor(
-        private readonly api: PedidosService
+        private readonly api: PedidosService,
+        private readonly toastr: ToastrService,
+        private readonly router: Router
     ) { }
 
     async resolve(route: ActivatedRouteSnapshot) {
-        const page = route.queryParams.page || 1;
-        const pedidos = await this.api.getPedidos(page).toPromise();
-        return pedidos;
+        try {
+            const page = route.queryParams.page || 1;
+            const pedidos = await this.api.getPedidos(page).toPromise();
+            return pedidos;
+        } catch (error) {
+            if (error instanceof HttpErrorResponse) {
+                if (error.status === 401) {
+                    this.toastr.warning('Seu tempo logado expirou ou você não tem permissão, logue novamente para acessar o sistema',
+                        'Login expirado');
+                    this.router.navigate(['/login'], {
+                        queryParams: {
+                            navigate: '/pedidos'
+                        }
+                    });
+                }
+            }
+        }
     }
 }
